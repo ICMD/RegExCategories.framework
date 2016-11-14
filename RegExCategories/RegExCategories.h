3df53d4dc4b0b9d9200bacbee09a3d74753042e2
Baoshan Sheng <sheng@icmd.org>
2016-11-14 16:41:55 +0800

    Initial Commit

---
+++ RegExCategories/RegExCategories.h
@@ -0,0 +1,19 @@
+//
+//  RegExCategories.h
+//  RegExCategories
+//
+//  Created by Baoshan Sheng on 11/14/16.
+//  Copyright © 2016 International Classical Music Database. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RegExCategories.
+FOUNDATION_EXPORT double RegExCategoriesVersionNumber;
+
+//! Project version string for RegExCategories.
+FOUNDATION_EXPORT const unsigned char RegExCategoriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegExCategories/PublicHeader.h>
+
+
